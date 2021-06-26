917c3822a0885079d63c090c6220faf3aec6a993
wiem689 <74822484+wiem689@users.noreply.github.com>
2021-06-26 16:41:00 +0200

    first commit...

---
+++ src/main/java/tn/gestion/projets/spring/GestionDeProjetsApplication.java
@@ -0,0 +1,13 @@
+package tn.gestion.projets.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeProjetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDeProjetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tn/gestion/projets/spring/GestionDeProjetsApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.gestion.projets.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeProjetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
